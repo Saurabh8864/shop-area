d4e0c3565efc1517a6f12bd0280126f9646b5a7a
Saurabh <singhs.saurabh4876@gmail.com>
2023-08-03 02:47:56 +0530

    final commit

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,17 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 app.use(cors({
-  origin: ['https://shop-area-irzl.vercel.app/',],
+  origin: 'https://shop-area-irzl.vercel.app',
   credentials: true
 }));
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/", (req, res) => {
+app.use("/",express.static(path.join(__dirname,"./uploads")));
+app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
 
