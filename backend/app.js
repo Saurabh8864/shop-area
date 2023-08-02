f069ddc0ee4371d549fa4086a28e5762d810efdd
Saurabh <singhs.saurabh4876@gmail.com>
2023-08-03 02:33:02 +0530

    frontend url done

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['http://localhost:3000',],
+  origin: ['https://shop-area-irzl.vercel.app/',],
   credentials: true
 }));
 
