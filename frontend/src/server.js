b94b05a4a7f453efaa10db1faf693f5491876e16
Saurabh <singhs.saurabh4876@gmail.com>
2023-08-03 03:36:52 +0530

    socket connection done

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
 export const server = "https://shop-area.vercel.app/api/v2";
-  export const backend_url ="https://shop-area.vercel.app/"
+  export const backend_url ="https://shop-area.vercel.app/";
 
 
